6ce1d71c9a2655c754dedc3d316e7803979e11ce
Sloan <sloanstewartdev@gmail.com>
2017-06-12 20:46:03 -0400

    code cleanup

--- index.js
+++ index.js
@@ -10,232 +10,228 @@ var marker = null;
 var radius = null;
 function initMap() {
     
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 33.7700000, lng: -84.3500000}, // Should be close to the center of the universe, er...Atlanta.
-      zoom: 10,
-      styles:[
-  {
-    "elementType": "geometry",
-    "stylers": [
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.7700000, lng: -84.3500000}, // Should be close to the center of the universe, er...Atlanta.
+    zoom: 10,
+    styles:[
       {
-        "color": "#f5f5f5"
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dadada"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c9c9c9"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
       }
     ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dadada"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#c9c9c9"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  }
-]
-    });
-    infoWindow = new google.maps.InfoWindow;
+  });
+  infoWindow = new google.maps.InfoWindow;
    
-   // This event listener calls addMarker() when the map is clicked.
-    google.maps.event.addListener(map, 'click', function(event) {
-      addMarker(event.latLng);
-      addRadius(event.latLng);
-    });
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng);
+    addRadius(event.latLng);
+  });
     
-    // Deletes previous marker and adds new marker to the map.
-    function addMarker(location) {
-        if (marker != null){
-            marker.setMap(null);
-        }
-        marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            draggable: true, // User can drag marker to new position
-            title: ''
-        });
-        marker.setMap(map);
-        setCoords();
-        $('.search-button').prop('disabled', false);
-        marker.addListener('dragend', function(event) {
-            setCoords();
-        });
+  // Deletes previous marker and adds new marker to the map.
+  function addMarker(location) {
+      if (marker != null){
+          marker.setMap(null);
+      }
+      marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          draggable: true, // User can drag marker to new position
+          title: ''
+      });
+      marker.setMap(map);
+      setCoords();
+      $('.search-button').prop('disabled', false);
+      marker.addListener('dragend', function(event) {
+          setCoords();
+      });
+  }
     
-    }
+  function addRadius(location){
+      console.info();
+      if (radius != null){
+          radius.setMap(null);
+      }
+      radius = new google.maps.Circle({
+          strokeColor: '#008596',
+          strokeOpacity: 0.25,
+          strokeWeight: 2,
+          fillColor: '#00bdd6',
+          fillOpacity: 0.15,
+          map: map,
+          center: location,
+          radius: Number($('.js-radius').val())
+       });
+       radius.bindTo("center", marker, "position"); // Radius is bound to Marker
+       console.log(radius.radius);
+  }
     
-    function addRadius(location){
-        console.info();
-        if (radius != null){
-            radius.setMap(null);
-        }
-        radius = new google.maps.Circle({
-            strokeColor: '#008596',
-            strokeOpacity: 0.25,
-            strokeWeight: 2,
-            fillColor: '#00bdd6',
-            fillOpacity: 0.15,
-            map: map,
-            center: location,
-            radius: Number($('.js-radius').val())
-         });
-         radius.bindTo("center", marker, "position"); // Radius is bound to Marker
-         console.log(radius.radius);
-    }
-    
-    function setCoords () {
-        // Get position of marker and round the lat & lng to numbers that work with Instagram API.
-        var lat = parseFloat(Math.round(marker.getPosition().lat() * 100) / 100).toFixed(7);
-        var lng = parseFloat(Math.round(marker.getPosition().lng() * 100) / 100).toFixed(7);
-        console.log('Marker set at '+lat+','+lng);
-        $('.js-lat').val(lat);;
-        $('.js-lng').val(lng);
-    }
+  function setCoords () {
+      // Get position of marker and round the lat & lng to numbers that work with Instagram API.
+      var lat = parseFloat(Math.round(marker.getPosition().lat() * 100) / 100).toFixed(7);
+      var lng = parseFloat(Math.round(marker.getPosition().lng() * 100) / 100).toFixed(7);
+      console.log('Marker set at '+lat+','+lng);
+      $('.js-lat').val(lat);;
+      $('.js-lng').val(lng);
+  }
 }
 // NOW LEAVING GOOGLE MAP MAGIC!
 
 var API_URL = 'https://api.instagram.com/v1/media/search';
-var QUERY_HISTORY = null;
-var NEXT_PAGE_TOKEN = null;
-var PREV_PAGE_TOKEN = null;
 var RESULT_HTML_TEMPLATE = (
   '<div class="result">' +
   	'<div class="js-user-info">' +
@@ -254,23 +250,20 @@ var RESULT_HTML_TEMPLATE = (
   '</div>'
 );
 
-function getDataFromApi(query, lat, lng, rad, callback) {
+function getDataFromApi(lat, lng, rad, callback) {
   var query = {
-    q: query,
     lat: lat,
     lng: lng,
     distance: rad,
     access_token: '5574247135.de4dc3c.19d7fe308e5145c4b2a9d83c233c3242',
     scope: 'public_content'
   };
-  // $.getJSON(API_URL, query, callback);
   $.ajax({
 	  dataType: "jsonp", //must do this for the Instagram API to work via client side
 	  url: API_URL,
 	  data: query,
 	  success: callback
 	});
-	QUERY_HISTORY = query;
 }
 
 function renderResult(result) {
@@ -303,62 +296,26 @@ function displayData(data) {
 				return renderResult(item);
 		});
 		$('.js-search-results').append(results);
-		// store page tokens to be used with buttons
-		NEXT_PAGE_TOKEN = data.nextPageToken;
-		PREV_PAGE_TOKEN = data.prevPageToken;
-		// if the tokens are stored, enable the buttons
-		if(NEXT_PAGE_TOKEN){$('.js-nextpage').prop('disabled', false);}
-		if(PREV_PAGE_TOKEN !== (null || undefined))
-			{$('.js-prevpage').prop('disabled', false);}
-		else{$('.js-prevpage').prop('disabled', true);}
 		$('.video').click(function(){this.paused?this.play():this.pause();});
 	}
 	else {
 		console.error('No good data found!');
 		$(".js-result-count").text('No posts found in specified area.');
 	}
-	
 }
 
 function watchButtons() {
-	$('.login-button').click(function(){
-		window.location.href = "https://api.instagram.com/oauth/authorize/?client_id=de4dc3c2635a47d7baa749ec8c1ccaa1&redirect_uri=https://sloanstewart.io/instlanta/search.html&response_type=token&scope=public_content";
-	});
-
-function scrollToResults(){
+  $('.js-search-form').submit(function(event) {
+    event.preventDefault();
+    $(".js-result-count, .js-search-results").empty(); // Clear count text and results
+    var lat = $('.js-lat').val();
+    var lng = $('.js-lng').val();
+    var rad = $('.js-radius').val();
+    getDataFromApi(lat, lng, rad, displayData);
+    // Scroll to results
 		$('html, body').animate({
-		scrollTop: $('.js-search-results').offset().top
+		  scrollTop: $('.js-search-results').offset().top
 		},1000);
-}
-
-	$('.js-search-form').submit(function(event) {
-		event.preventDefault();
-		if (marker === null){
-			alert('NO MARKER SET!');
-		}
-		else{
-		    $(".js-result-count").text(''); // Clear reults text
-			var queryTarget = $(event.currentTarget).find('.js-query');
-			var query = queryTarget.val();
-			var lat = $(event.currentTarget).find('.js-lat').val();
-			var lng = $(event.currentTarget).find('.js-lng').val();
-			var rad = $(event.currentTarget).find('.js-radius').val();
-			// clear out the input
-			queryTarget.val("");
-			// clear out search results
-			$('.js-search-results').empty();
-			getDataFromApi(query, lat, lng, rad, displayData);
-			scrollToResults();
-		}
-	});
-  	$('.js-nextpage').unbind().click(function(){
-		console.log('[More Results]');
-		getDataFromApi(QUERY_HISTORY, displayData, NEXT_PAGE_TOKEN);
-	});
- 	$('.js-prevpage').unbind().click(function(){
-		console.log('[Previous Results]');
-		getDataFromApi(QUERY_HISTORY, displayData, PREV_PAGE_TOKEN);
-		PREV_PAGE_TOKEN = null;
 	});
 }
 
